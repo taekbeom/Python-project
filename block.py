e05ec3fe9a071b876f4322cf1da0e80f68dbcdae
Олег <dark.blluesky@gmail.com>
2022-05-27 02:56:02 +0300

    For this map I've spent one week of my life

--- block.py
+++ block.py
@@ -2,11 +2,10 @@ import pygame
 
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self, x, y, groups):
+    def __init__(self, pos, groups, sprite_type, surface=pygame.Surface((16,16))):
         super().__init__(groups)
-        self.image = pygame.image.load('graphics/object.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft=(x, y))
-        self.overlap_pos = self.rect.inflate(0, -20)
+        self.sprite_type = sprite_type
+        self.image = surface
+        self.rect = self.image.get_rect(topleft=pos)
+        self.overlap_pos = self.rect.inflate(0, -2)
 
-        # for map
-        self.sprite_type = 'block'
--- camera.py
+++ camera.py
@@ -8,10 +8,20 @@ class SpritesCameraGroup(pygame.sprite.Group):
         self.display_surf = pygame.display.get_surface()
         self.x_pos = 0
         self.y_pos = 0
+        self.offset = pygame.math.Vector2()
+
+        # creating the lowest surface
+        self.earth_surface = pygame.image.load('graphics/TileMap/lowestsurface.png').convert()
+        self.earth_rect = self.earth_surface.get_rect(topleft=(0, 0))
 
     def sprite_build(self, player):
         self.x_pos = player.rect.centerx - self.display_surf.get_size()[0] // 2
         self.y_pos = player.rect.centery - self.display_surf.get_size()[1] // 2
+        self.offset = pygame.math.Vector2(self.x_pos, self.y_pos)
+
+        #drawing the lowest surface
+        earth_position = self.earth_rect.topleft - self.offset
+        self.display_surf.blit(self.earth_surface, earth_position)
 
         for sprite in sorted(self.sprites(), key=lambda sprite_elem: sprite_elem.rect.centery):
             position_x = sprite.rect.centerx - self.x_pos
